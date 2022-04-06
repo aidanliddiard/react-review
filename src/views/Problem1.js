0ad91d346609e9d822b040e26532499ab97a6bbc
aidanliddiard <aidanliddiard@gmail.com>
2022-04-06 09:22:08 -0700

    blue circle working

--- src/views/Problem1.js
+++ src/views/Problem1.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Shape from '../components/Shape/Shape';
 
 import './Problem1.css';
 
 export default function Problem1() {
+  const [color, setColor] = useState('red');
+
+  const handleBlue = async () => {
+    await setColor('blue');
+  };
+
   return (
     <div className="problem-container">
       <h2>Problem 1: Props and State</h2>
@@ -13,12 +19,14 @@ export default function Problem1() {
         state, event listeners and update props to make that happen.
       </div>
       <div className="shape-controls">
-        <Shape color="blue" type="circle" small />
+        <div className="blue-ciricle" onClick={handleBlue}>
+          <Shape color="blue" type="circle" small />
+        </div>
         <Shape color="purple" type="circle" small />
         <Shape color="green" type="circle" small />
       </div>
       <div className="selected-shape">
-        <Shape color="red" type="circle"></Shape>
+        <Shape color={color} type="circle"></Shape>
       </div>
     </div>
   );
